6e8e7496f7c6ee133caf86c2002f14ca25dff79f
Deblyn Prado <deblynprado@hotmail.com>
2018-01-26 16:40:40 -0200

    Moving JS to correct file

--- js/app.js
+++ js/app.js
@@ -4,3 +4,17 @@ $('.icon-delete').click(function(e) {
 	e.preventDefault();
 	$(this).closest('li').hide('slow', function(){ $target.remove(); });
 })
+
+var swiper = new Swiper('.swiper-container', {
+	slidesPerView: 3,
+	spaceBetween: 0,
+	centeredSlides: true,
+	initialSlide: 1,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		renderBullet: function (index, className) {
+			return '<span class="slider-control swiper-pagination-bullet"></span>';
+		}
+	},
+});
